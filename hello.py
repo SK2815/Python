ad173064778791001769bc23032e1a90373d62a0
SK2815 <simrankaur9492@gmail.com>
2019-02-02 18:36:45 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+#Do remember about the "main" function. If I change the main function name with any other name, then it wll run successfuly but won't produce anything. The other name would work if I import the file. The primary file will always work with "__name__" = "__main__"
+
 def main():
     print("Hello world")
 
