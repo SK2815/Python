2b5de4200718a64c5e8309ce0e2d8bd8ad17994b
SK2815 <simrankaur9492@gmail.com>
2019-02-02 21:38:50 -0500

    Add files via upload

---
+++ functions_start.py
@@ -0,0 +1,29 @@
+def myFunction():
+    print("heyyaahh I am a function")
+
+myFunction()
+#The below statement will call the function and will print the statement as usual.
+#BUT NOTE: after calling the myFunction, print will execute itself, because it is also a function.
+#So, it will first print the statement normally and then it will print "None", which is python constant
+#This happens because myFunction doesnt return anything
+#None is a python return constant
+print(myFunction()) 
+#This will print some bullshit address like 0x0034534, because this is not a function at all. this will print the value of function definition.
+#this means, the functions are objects to python code.
+print(myFunction)
+
+#this function uses value of x = 1 as its default value.
+def power(num,x=1):
+    result = 1
+    for i in range(x):
+        result = result*num
+    return result
+
+print(power(2))
+print(power(2,3))
+
+#Here is an interesting thing python lets us to do. You can switch the arguments and still get the same result.
+#THAT'S SO COOL. For that, we just need to give the name of the argument in any order, and python interpeter will figure out the correct value of the argument on its own.
+
+print(power(x=3,num = 2))
+#so, x is written first, which was the second argument. But it didn't matter to python interpreter because we gave the name.
